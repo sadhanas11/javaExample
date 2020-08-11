28f66403f001d3387021a3411ca37d58d8d291dc
Sadhana S <sadhanas11@gmail.com>
2020-08-11 14:25:34 +0530

    Initial Commit

---
+++ src/main/java/com/example/sample/hellexample/HellexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample.hellexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample/hellexample/HellexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample.hellexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
